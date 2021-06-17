7584e2aa564d6216e2380555c40b76933fd3f40c
vakulinina <lerrun@gmail.com>
2021-06-17 21:57:59 +0300

    refactor: refactor according to review

--- src/components/MoviesGrid/MoviesGrid.js
+++ src/components/MoviesGrid/MoviesGrid.js
@@ -3,47 +3,26 @@ import styles from './MoviesGrid.css';
 import MoviesGridItem from '../MoviesGridItem/MoviesGridItem';
 
 export default function MoviesGrid({ searchValue, searchResult, popularMovies }) {
-  let title;
-  let gridItems;
-
   if (!popularMovies) {
     return <p>Loading...</p>;
   }
 
-  if (searchValue) {
-    gridItems = (
-      <>
-        {searchResult.map(({ id, original_title, poster_path }) => (
-          <MoviesGridItem
-            key={id}
-            originalTitle={original_title}
-            posterPath={poster_path}
-            id={id}
-          />
-        ))}
-      </>
-    );
-    title = 'Search Result';
-  } else {
-    gridItems = (
-      <>
-        {popularMovies.map(({ id, original_title, poster_path }) => (
-          <MoviesGridItem
-            key={id}
-            originalTitle={original_title}
-            posterPath={poster_path}
-            id={id}
-          />
-        ))}
-      </>
-    );
-    title = 'Popular Movies';
-  }
+  const title = searchValue ? 'Search Result' : 'Popular Movies';
+  const movies = searchValue ? searchResult : popularMovies;
 
   return (
     <div className={styles['movies-list-wrap']}>
       <h3>{title}</h3>
-      <div className={styles['movies-list']}>{gridItems}</div>
+      <div className={styles['movies-list']}>
+        {movies.map(({ id, original_title, poster_path }) => (
+          <MoviesGridItem
+            key={id}
+            originalTitle={original_title}
+            posterPath={poster_path}
+            id={id}
+          />
+        ))}
+      </div>
     </div>
   );
 }
